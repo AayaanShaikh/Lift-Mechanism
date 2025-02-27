06e88da19dc281f917189932377a92a64fb0b5e9
AayaanShaikh <118344989+AayaanShaikh@users.noreply.github.com>
2025-02-27 11:55:17 +0530

    Update lift.py

--- lift.py
+++ lift.py
@@ -5,7 +5,7 @@ current_floor = 0
 
 while(True):
     floor_rn = int(input("Enter your current floor: "))
-    floor_req = int(input("Enter the floor you wish to go: ")
+    floor_req = int(input("Enter the floor you wish to go: "))
     if floor_rn < floor_req:
         up.append(floor_req)
     elif floor_rn > floor_req:
